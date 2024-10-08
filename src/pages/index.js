eed01ff2ae0edb39782fa08fc86cdfbcd12fca23
Timothy Nguyen <timn@celticchicago.com>
2024-10-08 11:21:23 -0500

    plugins and gatsby-browser.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from  'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage alt="Jake the Dog" src="../images/jake.jpg"/>
     </Layout>
   )
 }
